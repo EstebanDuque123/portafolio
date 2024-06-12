7d841fda8c1c2b2bf4ea23e6b20f31379c20082f
EstebanDuque123 <esteban_duque159@hotmail.com>
2024-06-12 15:39:35 -0500

    subida proyecto portafolio

---
+++ src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { PresentacionComponent } from './components/presentacion/presentacion.component';
+
+export const routes: Routes = [{
+path:'',
+component:PresentacionComponent
+}];
